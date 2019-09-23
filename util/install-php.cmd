6ef326b742b2f5c96774fc8b14a79d84eefb93f4
Lucas Schmitt <l.schmitt@wolpha.eu>
2019-09-23 15:44:21 +0200

    php is now downloaded from a remote source

--- util/install-php.cmd
+++ util/install-php.cmd
@@ -3,6 +3,25 @@ rem Created by Lucas Schmitt at TK-Schulsoftware
 @ECHO OFF
 call config.cmd
 
-util\unzip.exe util\install-php.zip
+echo === Downloading PHP-%PHP_VERSION% ===
+
+rem VisualStudio Version evaluation for PHP-%PHP_VERSION%
+
+set VC_VERSION=15
+IF /i %PHP_VERSION%==5.6.40 set VC_VERSION=11
+IF /i %PHP_VERSION%==7.0.33 set VC_VERSION=14
+IF /i %PHP_VERSION%==7.1.31 set VC_VERSION=14
+IF /i %PHP_VERSION%==7.2.21 set VC_VERSION=15
+IF /i %PHP_VERSION%==7.3.8 set VC_VERSION=15
+
+mkdir php
+cd php
+mkdir %PHP_VERSION%
+cd %PHP_VERSION%
+powershell -Command "Invoke-WebRequest https://windows.php.net/downloads/releases/archives/php-%PHP_VERSION%-nts-Win32-VC%VC_VERSION%-x64.zip -OutFile php-%PHP_VERSION%.zip"
+..\..\util\unzip.exe php-%PHP_VERSION%.zip
+del php-%PHP_VERSION%.zip
+cd ..
+cd ..
 
 msg "%username%"  Installed PHP successfully.
