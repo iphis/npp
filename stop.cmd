afab67792ac9ee1375df4230327e8d41d3129ed3
Lucas Schmitt <l.schmitt@wolpha.eu>
2019-09-23 13:16:08 +0200

    updated pgsql stop script for configurable a version

--- stop.cmd
+++ stop.cmd
@@ -4,6 +4,6 @@ rem Created by Lucas Schmitt at TK-Schulsoftware
 taskkill /f /IM nginx.exe
 taskkill /f /IM php-cgi.exe
 taskkill /f /IM pgAdmin4.exe
-pgsql\bin\pg_ctl.exe -D pgsql\data stop
+postgres\%PGSQL_VERSION%\bin\pg_ctl.exe -D postgres\%PGSQL_VERSION%\data stop
 
 msg "%username%"  Stopped Nginx and PHP successfully
