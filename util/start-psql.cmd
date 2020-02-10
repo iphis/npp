f203463b5237cfad81a2d6abb23678e51594b03e
Lucas Schmitt <l.schmitt@wolpha.eu>
2020-02-10 18:48:44 +0100

    Update start-psql.cmd

--- util/start-psql.cmd
+++ util/start-psql.cmd
@@ -1,10 +1,13 @@
 rem Created by Lucas Schmitt at TK-Schulsoftware
 
 echo === Starting PostgreSQL ===
-start "" postgres\%PGSQL_VERSION%\bin\pg_ctl.exe start -D postgres\%PGSQL_VERSION%\data -l postgres\%PGSQL_VERSION%\log
+start /B "" pgsql\bin\pg_ctl.exe start -D pgsql\data -l pgsql\log
 sleep 2
 echo Started PostgreSQL
+IF "%DISABLE_PGADMIN%"=="TRUE" (
+exit
+)
 echo === Starting PGAdmin 4 (PostgreSQL) ===
-util\RunHiddenConsole.exe "postgres\%PGSQL_VERSION%\pgAdmin 4\bin\pgAdmin4.exe"
+util\RunHiddenConsole.exe "pgsql\pgAdmin 4\bin\pgAdmin4.exe"
 echo Started pgAdmin4
 sleep 2
